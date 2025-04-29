eda32f4bd5f938a2b015a61c4e875bdc7ab35e0b
korobkad3v <korobka.d3v@gmail.com>
2025-04-29 16:37:02 +0300

    CRT EFFECTS
    [+] SDA Effect
    [+] Flicker Effect
    [+] Color separation Effect

--- frontend/src/components/section/Section.jsx
+++ frontend/src/components/section/Section.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import './Section.scss'
 import FrameWrapper from '../../assets/FrameWrapper.svg?react';
-import { Element } from "react-scroll";
 
 const Section = ({id, children}) => {
     return (
         
-        <section id={id} className="section-container"  >
-            <FrameWrapper className="section"/>
+        <section id={id} className="section">
+            <FrameWrapper className="section__frame"/>
             <div className="section-content">
                 <div className="section-content__inner">
                     {children}
                 </div>
             </div>
         </section>
-        
     );
 };
 
