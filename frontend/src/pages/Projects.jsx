e65f0b9168a769b60d05e2003809d9fc77192d85
korobkad3v <korobka.d3v@gmail.com>
2025-05-24 22:51:49 +0300

    feat: go back link from placeholder page

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
     return (
         <>
         <Section>
-            <h1>In development, but you can check my <a href="https://github.com/korobkad3v">github</a></h1>
+            <h1>In development, but you can check my <a href="https://github.com/korobkad3v">github</a> or go <a href="/">back</a></h1>
         </Section>
         </>
     );
